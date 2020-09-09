a576f2a2ab2b4c8b9a2ccc11350afffc65de0132
rafaelisaiasreis <rafaelisaiasreis@gmail.com>
2020-09-09 18:26:15 -0300

    Adiciona DB config e cria novo document

--- spring-webflux/src/main/java/com/example/springwebflux/SpringWebfluxApplication.java
+++ spring-webflux/src/main/java/com/example/springwebflux/SpringWebfluxApplication.java
@@ -1,13 +1,16 @@
 package com.example.springwebflux;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringWebfluxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebfluxApplication.class, args);
+		log.info("Hello World with Webflux");
 	}
 
 }
